ff9a3bf6185d60e9d6ac9183e3fe0d1dd9ca5a85
Dakostu <Dkostuj@gmx.de>
2017-11-01 16:45:16 +0100

    overhaul of regex filtering & searching

--- yogit.py
+++ yogit.py
@@ -5,48 +5,57 @@
 
 import webbrowser 
 import random
-import time
 import urllib
 import re
 from six.moves.urllib.request import urlopen
+from datetime import date, timedelta
 
-# Get current timestamp for advanced search
-currentDay = time.strftime("%d")
-currentMonth = time.strftime("%m")
-currentYear = time.strftime("%Y")
+# Get today timestamp for advanced search
+today = date.today()
+yesterday= date.today() - timedelta(1)
+todayDay = today.strftime("%d")
+todayMonth = today.strftime("%m")
+todayYear = today.strftime("%Y")
+yesterdayDay = yesterday.strftime("%d")
+yesterdayMonth = yesterday.strftime("%m")
+yesterdayYear = yesterday.strftime("%Y")
 gitHubURL = "https://github.com"
-urlLists = []
+urlList = []
 # RegEx blacklist 
-blacklist = "(?!assets|articles|site|images|open_graph|features|business|_private|docs|integrations|modules|pricing|feature|browser|search|trending|topics|unicode|devops|icons|pulls|issues|marketplace|explore|organizations|new)"
-# regular expression for "/username/name-of-repository"
-regEx = "/+" + blacklist + "[[\w]+/[\w\-\_\.]*]*"
+blacklist = "(?!assets|articles|site|images|open_graph|features|business|_private|docs|blog|about|integrations|modules|pricing|feature|browser|search|trending|topics|unicode|devops|icons|pulls|issues|marketplace|explore|organizations|site|security|github-external|new|contact|[\w\-\_\.]*github)"
+# regular expression for "/user-name/name-of-repository"
+regEx = "/+" + blacklist + "[[\w\-\_\.]+/[\w\-\_\.]*]*"
 
 # skim through first three search result pages
 # and filter out repository URL into a list
 for i in range(1,4):
     # URL for advanced search:
     # p=i: open page number i
-    # q=created: created on current day
+    # q=created: created on a certain day
     # s=updated: sort by most recent update
-    searchUrl = gitHubURL + "/search?p=" + str(i) + "&q=created%3A" + currentYear + "-" + currentMonth + "-" + currentDay + "&s=updated&type=Repositories"
+    searchUrl = gitHubURL + "/search?p=" + str(i) + "&q=created%3A" + todayYear + "-" + todayMonth + "-" + todayDay + "&q=created%3A" + yesterdayYear + "-" + yesterdayMonth + "-" + yesterdayDay + "&s=updated&type=Repositories"
     
     try:
         fileDesc = urlopen(searchUrl)
     except urllib.error.HTTPError as e:
         print ("HTTP Error: " + str(e.code))
         raise SystemExit
+    except urllib.error.URLError as e:
+        print ("Error while accessing Github pages:")
+        print (str(e.reason))
+        print ("Please check your connection status.")
+        raise SystemExit
     
     pagesStringified = fileDesc.read().decode()
-    urlLists += re.findall(regEx,pagesStringified)
+    urlList += re.findall(regEx,pagesStringified)
     
-if len(urlLists) == 0:
+if len(urlList) == 0:
            print ("No Repositories found.")
            raise SystemExit
     
-
 # get random element from list and open in browser
 random.seed()
-randomRepoLink = urlLists.pop(random.randrange(len(urlLists)))
+randomRepoLink = urlList.pop(random.randrange(len(urlList)))
 webbrowser.open(gitHubURL + randomRepoLink, new=2, autoraise=True)
 
 
